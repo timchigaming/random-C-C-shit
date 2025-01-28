5715f29f9da46e46e57815d7e6acbb313f08bd61
TimChi <123629996+timchigaming@users.noreply.github.com>
2025-01-28 22:20:58 +0300

    blah-blah

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,36 @@ void glfwKeyCallback(GLFWwindow* ptr_window, int key, int scancode, int action, 
 // Размеры создаваемого окна
 static int gl_windowSize_X(640), gl_windowSize_Y(480);
 
+static GLfloat points[] = {
+     0.f,   0.5f, 0.f,
+     0.5f, -0.5f, 0.f,
+    -0.5f, -0.5f, 0.f
+};
+
+static GLfloat colors[] = {
+    1.f, 0.f, 0.f,
+    0.f, 1.f, 0.f,
+    0.f, 0.f, 1.f
+};
+
+const char* vertex_shader =
+"#version 460\n"
+"layout(location = 0) in vec3 vertex_pos;"
+"layout(location = 1) in vec3 vertex_color;"
+"out vec3 color;"
+"void main(){"
+"   color = vertex_color;"
+"   gl_Position = vec4(vertex_pos, 1.0);"
+"}";
+
+const char* fragment_shader =
+"#version 460\n"
+"in vec3 color;"
+"out vec4 pixel_color;"
+"void main() {"
+"   pixel_color = vec4(color, 1.0);"
+"}";
+
 int main(void)
 {
 
@@ -53,11 +83,53 @@ int main(void)
 
     glClearColor(0.1, 0.3, 0.3, 0);
 
+    GLuint vs = glCreateShader(GL_VERTEX_SHADER);       //Создаём new VERTEX_шейдер
+    glShaderSource(vs, 1, &vertex_shader, nullptr);     //Говорим ему, шейдер vs(еденичка похуй) содержит в себе код из &vertex_shader;
+    glCompileShader(vs);                                //Ну, и, компилируем его
+
+    GLuint fs = glCreateShader(GL_FRAGMENT_SHADER);     //Создаём new FRAGMENT_шейдер
+    glShaderSource(fs, 1, &fragment_shader, nullptr);   //Говорим ему, шейдер fs(еденичка похуй) содержит в себе код из &fragment_shader;
+    glCompileShader(fs);                                //Ну, и, компилируем его.
+
+    GLuint shader = glCreateProgram();                  //Сама по себе шейдерная программа
+    glAttachShader(shader, vs);                         //+= vs
+    glAttachShader(shader, fs);                         //+= fs
+    glLinkProgram (shader);                             //LAZYADD(shader, vs, fs);
+
+    glDeleteShader(vs);                                 //Мы их скомпилировали, и потому, они нам нахуй больше не нужны. УДАЛЯЕМ.
+    glDeleteShader(fs);                                 //Мы их скомпилировали, и потому, они нам нахуй больше не нужны. УДАЛЯЕМ.
+
+    GLuint points_vbo = 0;                              // Массив из вертексов для видеокарты.
+    glGenBuffers(1, &points_vbo);
+    glBindBuffer(GL_ARRAY_BUFFER, points_vbo);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(points), points, GL_STATIC_DRAW);
+
+    GLuint colors_vbo = 0;
+    glGenBuffers(1, &colors_vbo);
+    glBindBuffer(GL_ARRAY_BUFFER, colors_vbo);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(colors), colors, GL_STATIC_DRAW);
+
+    GLuint vao = 0;
+    glGenVertexArrays(1, &vao);
+    glBindVertexArray(vao);
+
+    glEnableVertexAttribArray(0);
+    glBindBuffer(GL_ARRAY_BUFFER, points_vbo);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
+
+    glEnableVertexAttribArray(1);
+    glBindBuffer(GL_ARRAY_BUFFER, colors_vbo);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
+
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(ptr_window))
     {
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
+
+        glUseProgram(shader);
+        glBindVertexArray(vao);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
 
         /* Swap front and back buffers */
         glfwSwapBuffers(ptr_window);
@@ -90,6 +162,6 @@ void glfwKeyCallback(GLFWwindow* ptr_window, int key, int scancode, int action, 
 {
     if(key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
     {
-        glfwSetWindowShouldClose(ptr_window, true);
+        glfwSetWindowShouldClose(ptr_window, GL_TRUE);
     }
 }
