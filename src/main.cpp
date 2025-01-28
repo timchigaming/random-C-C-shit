1dfa5e1f3c2c02d314b2cc317148bb0b0f29ca19
TimChi <123629996+timchigaming@users.noreply.github.com>
2025-01-28 18:38:45 +0300

    blah-blah

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,7 @@
+#include <glad/glad.h>
 #include <GLFW/glfw3.h>
+
+#include <iostream>
 
 int main(void)
 {
@@ -19,11 +22,20 @@ int main(void)
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
 
+    if(!gladLoadGL())
+    {
+        std::cout << "CANT'T LOAD GLAD!" << std::endl;
+    }
+
+    std::cout << "OpenGl " << GLVersion.major << '.' << GLVersion.minor << std::endl;
+
+    glClearColor(0.1, 0.3, 0.3, 0);
+
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
         /* Render here */
-        //glClear(GL_COLOR_BUFFER_BIT);
+        glClear(GL_COLOR_BUFFER_BIT);
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
