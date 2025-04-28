2c31a64662a65243408ed0bfc771b62425d90f3f
TimChi <timachigileichik@gmail.com>
2025-04-28 11:06:48 +0300

    No GovnnoCode Allowed. Fixing

--- Proj_ Flickering Lights/winapi.c
+++ Proj_ Flickering Lights/winapi.c
@@ -10,9 +10,11 @@ typedef struct Desktop{
 	LPRECT rect;
 } Desktop;
 
-void MakePIZDEC();
-void HideAll();
 Desktop* InitDesktop();
+void MakePIZDEC();
+void DrawCountdown(clock_t *now, char *elapsed, char *cooldown, Desktop* desktop, HFONT *fancyText);
+void ToggleDesktopWallPaper(BOOL relative, const char* path);
+void HideAll();
 
 int main() {
 	ShowWindow(GetConsoleWindow(), SW_HIDE);
@@ -22,83 +24,11 @@ int main() {
 			MakePIZDEC();
 		}
 		MessageBox(NULL, "Вот так мы постепенно и учимся всякой хуйне.", "блеблебле", MB_OK);
+		ToggleDesktopWallPaper(TRUE, "CachedImage_1920_1080_POS4.jpg");
 
-		char dstPath[MAX_PATH] = {};
-		GetCurrentDirectory(MAX_PATH, dstPath);
-	
-		strcat(dstPath, "\\");
-		strcat(dstPath, "CachedImage_1920_1080_POS4.jpg");
-
-		SystemParametersInfo(SPI_SETDESKWALLPAPER, 0, (PVOID)dstPath, SPIF_UPDATEINIFILE);
 		return 1;
 	}
-
 	return 0;
-}
-
-void MakePIZDEC() {
-	Desktop* desktop = InitDesktop();
-
-	// Меняем обои дабы было смешно.
-	char srcPath[MAX_PATH];
-	DWORD len = ExpandEnvironmentStrings(
-		TEXT("%AppData%\\Microsoft\\Windows\\Themes\\CachedFiles\\CachedImage_1920_1080_POS4.jpg"),
-		srcPath, MAX_PATH
-	);
-	CopyFile(srcPath, "CachedImage_1920_1080_POS4.jpg", FALSE);
-
-	char dstPath[MAX_PATH] = {};
-    GetCurrentDirectory(MAX_PATH, dstPath);
-
-    strcat(dstPath, "\\");
-    strcat(dstPath, "ZRYA.png");
-
-	SystemParametersInfo(SPI_SETDESKWALLPAPER, 0, (PVOID)dstPath, SPIF_UPDATEINIFILE);
-	HideAll();
-
-	// Сделаем красивый шрифт.
-	#define fontH 80
-	#define fontW 30
-	HFONT fancyText = CreateFontW(
-		fontH, fontW, 0, 0, FW_MEDIUM,
-		FALSE, FALSE, FALSE, DEFAULT_CHARSET,
-		OUT_OUTLINE_PRECIS, CLIP_DEFAULT_PRECIS, ANTIALIASED_QUALITY,
-		FF_DECORATIVE, (LPCWSTR)"Comic Sans"
-	);
-	
-	SetTextColor(desktop->hdc, RGB(255, 255, 255));
-	SetBkColor(desktop->hdc, RGB(0, 0, 0));
-	SelectObject(desktop->hdc, GetStockObject(DC_BRUSH));
-	SetDCBrushColor(desktop->hdc, RGB(0,0,0));
-	
-	clock_t now = clock();
-	char elapsed = 0;
-	char cooldown = 10;
-
-	while(elapsed < cooldown) {
-		elapsed = (clock() - now)/CLOCKS_PER_SEC;
-
-		SetCursorPos(0, desktop->resolutionY);
-
-		SelectObject(desktop->hdc, GetStockObject(DC_BRUSH));
-		Rectangle(desktop->hdc, 0, 0, desktop->resolutionX, desktop->resolutionY);
-
-		char countdown[2] = {};
-		sprintf(countdown, "%d", cooldown - elapsed);
-		SelectObject(desktop->hdc, fancyText);
-		TextOut(desktop->hdc,
-			desktop->resolutionX / 2 - fontW/2,
-			desktop->resolutionY / 2 - fontH/2,
-			countdown,
-			2
-		);
-	}
-
-	ReleaseDC(desktop->hwnd, desktop->hdc);
-	SetCursorPos(desktop->resolutionX / 2 + 10, desktop->resolutionY / 2 + 46); // Курсор будет четко на кнопке OK
-	free(desktop->rect); desktop->rect = 0;
-	free(desktop); desktop = 0;
-	return;
 }
 
 Desktop* InitDesktop() {
@@ -115,8 +45,94 @@ Desktop* InitDesktop() {
 	desktop->resolutionY = desktop->rect->bottom;
 }
 
-void HideAll()
-{
+void MakePIZDEC() {
+	Desktop* desktop = InitDesktop();
+
+	ToggleDesktopWallPaper(TRUE, "ZRYA.png");
+	HideAll();
+
+	// Сделаем красивый шрифт.
+	#define fontH 80
+	#define fontW 30
+	HFONT fancyText = CreateFontW(
+		fontH, fontW, 0, 0, FW_MEDIUM,
+		FALSE, FALSE, FALSE, DEFAULT_CHARSET,
+		OUT_OUTLINE_PRECIS, CLIP_DEFAULT_PRECIS, ANTIALIASED_QUALITY,
+		FF_DECORATIVE, (LPCWSTR)"Comic Sans"
+	);
+	
+	clock_t now = clock();
+	char elapsed = 0;
+	char cooldown = 3;
+
+	while(elapsed < cooldown) {
+		DrawCountdown(&now, &elapsed, &cooldown, desktop, &fancyText);
+	}
+
+	ReleaseDC(desktop->hwnd, desktop->hdc);
+	SetCursorPos(desktop->resolutionX / 2 + 10, desktop->resolutionY / 2 + 46); // Курсор будет четко на кнопке OK
+	free(desktop->rect); desktop->rect = 0;
+	free(desktop); desktop = 0;
+	return;
+}
+
+void DrawCountdown(clock_t *now, char *elapsed, char *cooldown, Desktop* desktop, HFONT *fancyText) {
+	*elapsed = (clock() - *now)/CLOCKS_PER_SEC;
+
+	SetCursorPos(0, desktop->resolutionY);
+
+	HDC memDC = CreateCompatibleDC(desktop->hdc);
+	HBITMAP memBM = CreateCompatibleBitmap(desktop->hdc, desktop->resolutionX, desktop->resolutionY);
+	SelectObject(memDC, memBM);
+
+	SetTextColor(memDC, RGB(255, 255, 255));
+	SetBkColor(memDC, RGB(0, 0, 0));
+	SelectObject(memDC, GetStockObject(DC_BRUSH));
+	SetDCBrushColor(memDC, RGB(0,0,0));
+
+	Rectangle(memDC, 0, 0, desktop->resolutionX, desktop->resolutionY);
+
+	char countdown[2] = {};
+	sprintf(countdown, "%d", *cooldown - *elapsed);
+	SelectObject(memDC, *fancyText);
+	TextOut(memDC,
+		desktop->resolutionX / 2 - fontW/2,
+		desktop->resolutionY / 2 - fontH/2,
+		countdown,
+		2
+	);
+
+	BitBlt(desktop->hdc, 0, 0, desktop->resolutionX, desktop->resolutionY, memDC, 0, 0, SRCCOPY);
+	DeleteDC(memDC);
+	DeleteObject(memBM);
+
+	return;
+}
+
+
+void ToggleDesktopWallPaper(BOOL relative, const char* path) {
+	// Меняем обои дабы было смешно.
+	char srcPath[MAX_PATH];
+	DWORD len = ExpandEnvironmentStrings(
+		TEXT("%AppData%\\Microsoft\\Windows\\Themes\\CachedFiles\\CachedImage_1920_1080_POS4.jpg"),
+		srcPath, MAX_PATH
+	);
+
+	CopyFile(srcPath, "CachedImage_1920_1080_POS4.jpg", FALSE);
+
+	char dstPath[MAX_PATH] = {};
+	if(relative) {
+		GetCurrentDirectory(MAX_PATH, dstPath);
+
+		strcat(dstPath, "\\");
+		strcat(dstPath, path);
+	} else {
+		strcpy(dstPath, path);
+	}
+	SystemParametersInfo(SPI_SETDESKWALLPAPER, 0, (PVOID)dstPath, SPIF_UPDATEINIFILE);
+}
+
+void HideAll() {
     keybd_event(VK_LWIN, 0, 0, 0);
     keybd_event('M', 0, 0, 0);
 
